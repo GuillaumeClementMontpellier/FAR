51892b0a5a7c2e287ef5ab5ab5651d2e427cd7a0
Etudiant IG <polig@igtp5-pc02.isim.intra>
2019-04-03 17:16:12 +0200

    OK

--- client.c
+++ client.c
@@ -6,41 +6,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <signal.h>
 
-int main(int argc, char* argv[]) // client
+//a besoin de -pthread pour compiler
+
+int dS;
+
+int tailleMax;
+
+void *recevoir()
 {
-  int tailleMax = 2000;
-  
-  if (argc < 2)
-  {
-    printf("Probleme d'arguments\nFormat : ./client \"IP\" \"port\" (facultatif : <\"nomFichierSource\")\n");
-    return 1;
-  }
-  
-  //Creer socket
-  int dS = socket(PF_INET,SOCK_STREAM,0);
-  
-  //Demander une connexion
-  struct sockaddr_in ad;
-  ad.sin_family = AF_INET;
-  ad.sin_port = htons(atoi(argv[2])); // port du receveur/serveur
-  
-  inet_pton(AF_INET,argv[1],&(ad.sin_addr)); //associe l'adresse ip correspondant à la chaine de char, et la mets dans ad.sin_addr
-  socklen_t lgA = sizeof(struct sockaddr_in);
-  connect(dS,(struct sockaddr *)&ad,lgA);
-  
-  //Communiquer
   //crée la variable pour l'envoi
   char* msg = (char*) malloc(tailleMax * sizeof(char));
   msg[0]='\0';
-
-  int fini = 0;
-
-  while(fini==0) // tant que pas fin
+  
+  while(1)
   {
-
-    printf("-------------- Vous allez recevoir un message --------------\n");
-        
     //Etape : recevoir la reponse (nombre d'octet) pas besoin ici
     int rep = recv(dS,msg,tailleMax,0);
     
@@ -52,37 +34,86 @@ int main(int argc, char* argv[]) // client
     {
       printf("Serveur deconnecte \n");
     }
-
-    printf("%s\n",msg);
+    
+    printf("Votre correspondant vous dit : %s\n",msg);
     
     if (strcmp(msg,"fin")==0) // check fin
     {
-      fini = 1;
-    }
-    
-    if(fini == 0) // si le message recu n'est pas fin
-    {
-
-      printf("-------------- Repondez --------------\n");
-    
-      fgets(msg,tailleMax,stdin);//message 1
-      char *pos = strchr(msg,'\n');
-      *pos = '\0';
-      
-      //Etape : envoyer
-      send(dS,msg,strlen(msg)+1,0); // +1 pour envoyer le \0
-      
-      if (strcmp(msg,"fin")==0) // check fin
-      {
-	fini = 2;
-      }
-      
+      free(msg);
+      raise(SIGUSR1); // fini le programme
     }
   }
-
-  free(msg);
+    
+}
+void *envoyer()
+{
+  //crée la variable pour l'envoi
+  char* msg = (char*) malloc(tailleMax * sizeof(char));
+  msg[0]='\0';
   
-  //Fermer
-  close(dS);
+  while(1)
+  {
+    printf("-------------- Repondez --------------\n");
+    
+    fgets(msg,tailleMax,stdin);//message 1
+    char *pos = strchr(msg,'\n');
+    *pos = '\0';
+    
+    //Etape : envoyer
+    send(dS,msg,strlen(msg)+1,0); // +1 pour envoyer le \0
+    
+    if (strcmp(msg,"fin")==0) // check fin
+    {
+      free(msg);
+      raise(SIGUSR1);
+    }
+     
+  }
+}
 
+void fin()
+{
+  close(dS);
+  exit(0);
+}
+
+int main(int argc, char* argv[]) // client
+{  
+  signal(SIGUSR1,fin); // signal pour finir
+  
+  if (argc < 2)
+  {
+    printf("Probleme d'arguments\nFormat : ./client \"IP\" \"port\" (facultatif : <\"nomFichierSource\")\n");
+    return 1;
+  }
+  
+  //Creer socket
+  dS = socket(PF_INET,SOCK_STREAM,0);
+
+  tailleMax = 2000;
+  
+  //Demander une connexion
+  struct sockaddr_in ad;
+  ad.sin_family = AF_INET;
+  ad.sin_port = htons(atoi(argv[2])); // port du receveur/serveur
+  
+  inet_pton(AF_INET,argv[1],&(ad.sin_addr)); //associe l'adresse ip correspondant à la chaine de char, et la mets dans ad.sin_addr
+  socklen_t lgA = sizeof(struct sockaddr_in);
+  connect(dS,(struct sockaddr *)&ad,lgA);
+  
+  //Communiquer
+
+  //Separer la lecture et l'ecriture en fonction, puis les appeler en thread => plus de 'fini' => signal pour finir
+  
+  pthread_t tRecevoir;
+  pthread_t tEnvoyer;
+
+  pthread_create(&tRecevoir,0,recevoir,0);
+  pthread_create(&tEnvoyer,0,envoyer,0);
+
+  pthread_join(tRecevoir,0);
+  pthread_join(tEnvoyer,0);
+
+  return 0;
+  
 }
--- serveur.c
+++ serveur.c
@@ -7,12 +7,118 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <pthread.h>
 
-//FONCTION QUI TRAITE LES CTRL+C
+ // Besoin de -pthread pour compiler
 
 int dS1;
+int dSC1;
+int dSC2;
 
-void fin()
+int tailleMax;
+
+void *c1v2()
+{
+  int taille;
+
+  char* msg = (char*) malloc((tailleMax+1) * sizeof(char)); // recoit le message
+
+  int fini = 0;
+  
+  while(fini == 0)
+  {
+    taille = recv(dSC1,msg,tailleMax,0);
+    
+    if (taille < 0)
+    { //gestion des erreurs
+      printf("Reception finale du message trop long \n");
+      fini = 1;
+    }
+    else if (taille == 0)
+    {
+      printf("Client deconnecte : End Of Message \n");
+      fini = 1;
+    }
+    else // pas de probleme lors de la reception, on envoie au client 2
+    {
+      
+      if (taille == tailleMax) // pour etre sur d'afficher tout le message, et pas depasser
+      {
+	char *pos = strchr(msg,'\0');
+	*pos = ' ';
+	msg[tailleMax] = '\0';
+      }
+
+      //renvoie au client 2
+      int rep = send(dSC2,msg,strlen(msg)+1,0);
+      
+      if (rep < 0)//gestion des erreurs
+      {
+	printf("Serveur : erreur lors du send de reponse : client plus disponible \n");
+	fini = 1; // client plus disponible
+      }
+      else if (rep == 0)
+      {
+	printf("Serveur : Client deconnecte avant reponse \n");
+	fini = 1;
+      }
+    }
+  }
+  return 0;
+}
+
+
+void *c2v1()
+{
+  int taille;
+
+  char* msg = (char*) malloc((tailleMax+1) * sizeof(char)); // recoit le message
+
+  int fini = 0;
+  
+  while(fini == 0)
+  {
+    taille = recv(dSC2,msg,tailleMax,0);
+    
+    if (taille < 0)
+    { //gestion des erreurs
+      printf("Reception finale du message trop long \n");
+      fini = 1;
+    }
+    else if (taille == 0)
+    {
+      printf("Client deconnecte : End Of Message \n");
+      fini = 1;
+    }
+    else // pas de probleme lors de la reception, on envoie au client 2
+    {
+      
+      if (taille == tailleMax) // pour etre sur d'afficher tout le message, et pas depasser
+      {
+	char *pos = strchr(msg,'\0');
+	*pos = ' ';
+	msg[tailleMax] = '\0';
+      }
+
+      //renvoie au client 2
+      int rep = send(dSC1,msg,strlen(msg)+1,0);
+      
+      if (rep < 0)//gestion des erreurs
+      {
+	printf("Serveur : erreur lors du send de reponse : client plus disponible \n");
+	fini = 1; // client plus disponible
+      }
+      else if (rep == 0)
+      {
+	printf("Serveur : Client deconnecte avant reponse \n");
+	fini = 1;
+      }
+    }
+  }
+  return 0;
+}
+
+void fin() //FONCTION QUI TRAITE LES CTRL+C
 {
   printf("\n Au revoir \n");
   //Fermer
@@ -25,7 +131,7 @@ int main(int argc, char* argv[]) // serveur
 {  
   signal(SIGINT,fin);
   
-  const int tailleMax = 2000;
+  tailleMax = 2000;
   
   //Creer socket
   dS1 = socket(PF_INET,SOCK_STREAM,0); // dedoublé pour les 2 clients
@@ -65,6 +171,7 @@ int main(int argc, char* argv[]) // serveur
   printf("Serveur : mon numero de port est : %d \n",  ntohs(ad1.sin_port));
   
   //Attendre un clients
+
   char* msg = (char*) malloc((tailleMax+1) * sizeof(char)); // recoit le message
   
   while(1)
@@ -74,7 +181,7 @@ int main(int argc, char* argv[]) // serveur
     printf("J'attend un client 1\n");
     struct sockaddr_in aC1; // adresse client
     socklen_t lg1 = sizeof(struct sockaddr_in);
-    int dSC1 = accept(dS1,(struct sockaddr*)&aC1,&lg1); // accepter la connexion client
+    dSC1 = accept(dS1,(struct sockaddr*)&aC1,&lg1); // accepter la connexion client
     
     if (dSC1 < 0)
     {//gere les erreurs
@@ -84,21 +191,11 @@ int main(int argc, char* argv[]) // serveur
     
     printf("Client 1 connecté : %s:%d \n",inet_ntoa(aC1.sin_addr), ntohs(aC1.sin_port));
 
-    
-    int rep = send(dSC1,msg,strlen(msg),0);
-    if (rep < 0)//gestion des erreurs
-    {
-      printf("Serveur : erreur lors du send du message : client pas disponible \n");
-    }
-    else if (rep == 0)
-    {
-      printf("Serveur : Client deconnecte avant reponse \n");
-    }
-    
+        
     printf("J'attend un client 2\n");
     struct sockaddr_in aC2; // adresse client
     socklen_t lg2 = sizeof(struct sockaddr_in);
-    int dSC2 = accept(dS1,(struct sockaddr*)&aC2,&lg2); // accepter la connexion client
+    dSC2 = accept(dS1,(struct sockaddr*)&aC2,&lg2); // accepter la connexion client
     
     if (dSC2 < 0)
     {//gere les erreurs
@@ -108,89 +205,17 @@ int main(int argc, char* argv[]) // serveur
     
     printf("Client 2 connecté : %s:%d \n",inet_ntoa(aC2.sin_addr), ntohs(aC2.sin_port));
 
-    int fini = 0;
-    int taille = 0;
+    // FAIRE LES PTHREAD
+    pthread_t tc1v2;
+    pthread_t tc2v1;
     
-    while (fini == 0){
-      
-      taille = recv(dSC1,msg,tailleMax,0);
-      
-      if (taille < 0)
-      { //gestion des erreurs
-	printf("Reception finale du message trop long \n");
-	fini = 1;
-      }
-      else if (taille == 0)
-      {
-	printf("Client deconnecte : End Of Message \n");
-	fini = 1;
-      }
-      else // pas de probleme lors de la receptioni
-      {
-	
-	if (taille == tailleMax) // pour etre sur d'afficher tout le message, et pas depasser
-	{
-	  char *pos = strchr(msg,'\0');
-	  *pos = ' ';
-	  msg[tailleMax] = '\0';
-	}
+    pthread_create(&tc1v2,0,c1v2,0);
+    pthread_create(&tc2v1,0,c2v1,0);
 
-	//renvoie au client 2
-	int rep = send(dSC2,msg,strlen(msg)+1,0);
-	
-	if (rep < 0)//gestion des erreurs
-	{
-	  printf("Serveur : erreur lors du send de reponse : client plus disponible \n");
-	  fini = 1; // client plus disponible
-	}
-	else if (rep == 0)
-	{
-	  printf("Serveur : Client deconnecte avant reponse \n");
-	  fini = 1;
-	}
-	
-      }
-
-      //puis pareil pour client 2 : echange client 1 et 2
-      taille = recv(dSC2,msg,tailleMax,0);
-      
-      if (taille < 0)
-      { //gestion des erreurs
-	printf("Reception finale du message trop long \n");
-	fini = 1;
-      }
-      else if (taille == 0)
-      {
-	printf("Client deconnecte : End Of Message \n");
-	fini = 1;
-      }
-      else // pas de probleme lors de la receptioni
-      {
-	
-	if (taille == tailleMax) // pour etre sur d'afficher tout le message, et pas depasser
-	{
-	  char *pos = strchr(msg,'\0');
-	  *pos = ' ';
-	  msg[tailleMax] = '\0';
-	}
-
-	//renvoie au client 1
-	int rep = send(dSC1,msg,strlen(msg)+1,0); // +1 pour le \0
-	
-	if (rep < 0)//gestion des erreurs
-	{
-	  printf("Serveur : erreur lors du send de reponse : client plus disponible \n");
-	  fini = 1; // client plus disponible
-	}
-	else if (rep == 0)
-	{
-	  printf("Serveur : Client deconnecte avant reponse \n");
-	  fini = 1;
-	}
-	
-      }
-      
-    }
+    // PTRHEAD WAIT FIN DES CLIENTS !!
+    
+    pthread_join(tc1v2,0);
+    pthread_join(tc2v1,0);
 
     //fermeture socket client
     
@@ -200,11 +225,5 @@ int main(int argc, char* argv[]) // serveur
     close(dSC2);
     
   }
-	
-  free(msg);
-  
-  printf("\n Au revoir \n");
-  //Fermer
-  close(dS1);
 
 }
